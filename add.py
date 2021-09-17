525eca92f45b187ba124e12a217d2a6e12b53ed5
Owen Spicker <ospicker@hotmail.com>
2021-09-17 13:27:18 -0700

    added division

---
+++ add.py
@@ -0,0 +1,15 @@
+
+print('Enter 2 numbers')
+
+x = input()
+y = input()
+
+add = int(x) + int(y)
+print('The sum of the numbers is ',  add)
+
+
+sub = int(x) - int(y)
+print('your first number minus your second number equals', sub)
+
+div = int(x)/int(y)
+print('your first number divided by your second number equals', div)
